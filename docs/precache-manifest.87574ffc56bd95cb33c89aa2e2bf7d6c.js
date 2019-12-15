b909c407b19682429849586ee5ab9af28b677dbc
Bipul Raman (MSFT) <bipulraman@gmail.com>
2019-12-15 21:28:46 +0530

    build folder created

---
+++ docs/precache-manifest.87574ffc56bd95cb33c89aa2e2bf7d6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf52964342ec13c657c0786c913c774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a63ee70d0caac4defef6",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "d6cb8d10fa355964d5a7",
+    "url": "/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "a63ee70d0caac4defef6",
+    "url": "/static/js/2.e30020fd.chunk.js"
+  },
+  {
+    "revision": "ef64eabdb9f5ee81f6c27a2bc01ab72e",
+    "url": "/static/js/2.e30020fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6cb8d10fa355964d5a7",
+    "url": "/static/js/main.c8639008.chunk.js"
+  },
+  {
+    "revision": "ebeedc31eaddf2971c53",
+    "url": "/static/js/runtime-main.2c81bb48.js"
+  }
+]);
